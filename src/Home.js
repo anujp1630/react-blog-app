6fd7d6fa5c360eaee20ac4fb9e7dc3e0487fc5cf
anujp1630 <anujp9868@gmail.com>
2024-01-10 21:26:24 +0530

    added pages for blogs

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import { useState ,useEffect} from "react";
+import Bloglist from "./Bloglist";
+const Home = () => {
+
+    const [blogs ,setblogs]=useState([
+
+        {title:'my new website',body:'lorem ipsum...',author:'mario',id:1},
+        {title:'welcome party',body:'lorem ipsum...',author:'yoshi',id:2},
+        {title:'web dev top tips',body:'lorem ipsum...',author:'mario',id:3}
+
+
+    ]) ;  
+    
+
+
+    const handleDelete=(id) =>{
+        const newBlogs=blogs.filter(blog=> blog.id!==id);
+         setblogs(newBlogs);
+    }
+
+    useEffect(() => {
+
+        console.log("use effect run");
+        
+    } , []);
+
+    return ( 
+
+        <div className="Home">
+            <Bloglist blogs={blogs} title="ALL BLOGS" handleDelete={handleDelete}></Bloglist>
+            
+        </div>
+           
+        
+     );
+}
+ 
+export default Home;
